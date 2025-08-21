6b255a7126a1506cce7dfd7c8aa2cf0f1f7f5344
annattaf <anna.avanco.ferreira@escola.pr.gov.br>
2025-08-21 14:29:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Japão?',
+    'A capital do Japão é Tóquio.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o órgão responsável pelo bombeamento de sangue no corpo humano?',
+    'O órgão responsável é o coração.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o valor de π (pi) até duas casas decimais?',
+    'O valor de π é aproximadamente 3,14.'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "Dom Casmurro"?',
+    'A obra foi escrita por Machado de Assis.'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou a Mona Lisa?',
+    'A Mona Lisa foi pintada por Leonardo da Vinci.'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a data da Independência do Brasil?',
+    'A Independência do Brasil foi proclamada em 7 de setembro de 1822.'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a unidade de medida da força no Sistema Internacional?',
+    'A unidade de medida da força é o Newton (N).'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o símbolo químico do ouro?',
+    'O símbolo químico do ouro é Au.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continente é a Ásia.'
+)
+
+criaCartao(
+    'Esportes',
+    'Quem venceu a Copa do Mundo de Futebol de 2018?',
+    'A seleção da França venceu a Copa do Mundo de 2018.'
+)
